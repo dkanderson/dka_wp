589967ac53d84f320843a1be4f192232ab91d6e8
dkanderson <dkanderson.designs@gmail.com>
2014-08-13 13:57:02 -0400

    add font, icons linf references

--- templates/head.php
+++ templates/head.php
@@ -8,5 +8,10 @@
 
   <?php wp_head(); ?>
 
+  <link href='//fonts.googleapis.com/css?family=Open+Sans:100,300,400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Goudy+Bookletter+1911' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300' rel='stylesheet' type='text/css'>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/favicon.png">
+  <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/touch-icon.png">
 </head>
