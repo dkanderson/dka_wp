53d420bc1e481b82f9751fdaf7f87beb052bb6c4
dkanderson <dkanderson.designs@gmail.com>
2014-08-13 01:12:28 -0400

    undo a terrible wrong

--- lib/scripts.php
+++ lib/scripts.php
@@ -20,14 +20,24 @@ function roots_scripts() {
    * Read the asset names from assets-manifest.json
    */
   
-  $get_assets = file_get_contents(get_template_directory() . '/assets/manifest.json');
-  $assets     = json_decode($get_assets, true);
-  $assets     = array(
-    'css'       => '/assets/css/main.min.css?' . $assets['assets/css/main.min.css']['hash'],
-    'js'        => '/assets/js/scripts.min.js?' . $assets['assets/js/scripts.min.js']['hash'],
-    'modernizr' => '/assets/js/vendor/modernizr.min.js',
-    'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'
-  );
+  if (WP_ENV === 'development') {
+    $assets = array(
+      'css'       => '/assets/css/main.css',
+      'js'        => '/assets/js/scripts.js',
+      'modernizr' => '/assets/vendor/modernizr/modernizr.js',
+      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js'
+    );
+  } else {
+    $get_assets = file_get_contents(get_template_directory() . '/assets/manifest.json');
+    $assets     = json_decode($get_assets, true);
+    $assets     = array(
+      'css'       => '/assets/css/main.min.css?' . $assets['assets/css/main.min.css']['hash'],
+      'js'        => '/assets/js/scripts.min.js?' . $assets['assets/js/scripts.min.js']['hash'],
+      'modernizr' => '/assets/js/vendor/modernizr.min.js',
+      'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'
+    );
+  }
+
   
 
   wp_enqueue_style('roots_css', get_template_directory_uri() . $assets['css'], false, null);
