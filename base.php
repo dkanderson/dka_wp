c74385f8edb165036bf924fe85bb5675de016f05
dkanderson <dkanderson.designs@gmail.com>
2014-08-13 13:58:35 -0400

    clean up

--- base.php
+++ base.php
@@ -1,5 +1,6 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
+<?php include 'assets/img/svg/sprites.svg' ?>
 
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
@@ -12,18 +13,9 @@
     get_template_part('templates/header');
   ?>
 
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <main class="main <?php echo roots_main_class(); ?>" role="main">
-        <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->
+  
+  <?php include roots_template_path(); ?>
+  
 
   <?php get_template_part('templates/footer'); ?>
 
