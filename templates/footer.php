28e69151f39e98d8f19cc9cabfbeb25010724053
dkanderson <dkanderson.designs@gmail.com>
2014-08-18 09:14:25 -0400

    personalise footer and tweaks

--- templates/footer.php
+++ templates/footer.php
@@ -1,21 +1,28 @@
 <section id="footer" role="footer">
     <footer class="container">
         <div class="one-third column">
-            <h3>Resume</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu. Suspendisse potenti. Nulla molestie scelerisque risus eget vulputate.</p>
+            <h3>Quick About.</h3>
+            <a title="Learn more about Duane Anderson" href="<?php bloginfo('url'); ?>/bio" class="img-wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/profile.jpg" alt=""></a>
+            <p>That's me. On a good day. The rest of the time I am wearing jeans and a t-shirt, coding, designing or hanging with my son. <a href="<?php bloginfo('url') ?>/bio" title="more about Duane">more here</a></p>
         </div>
         <div class="one-third column">
             <h3>Contact Me</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu. Suspendisse potenti. Nulla molestie scelerisque risus eget vulputate.</p>
+            <p>I am always interested in opinions that help me improve, so if you have ideas or just want to connect about a potential project, reach out. The best way to contact me is via <a href="mailto:duane@dkanderson.com" title="Send me an email">email</a>.</p>
         </div>
         <div class="one-third column">
             <h3>Social</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat arcu. Suspendisse potenti. Nulla molestie scelerisque risus eget vulputate.</p>
+            <p>I try my best at social networking when I can but I should warn you I am not very good at it. My feable attempt are summed up below: </p>
+            <nav class="social-icons">
+                <a href="http://twitter.com/dkanderson"><svg class="icon icon-twitter" viewBox="0 0 32 32"><use xlink:href="#icon-twitter"></use></svg></a>
+                <a href="http://instagram.com/webmatrixja"><svg class="icon icon-instagram" viewBox="0 0 32 32"><use xlink:href="#icon-instagram"></use></svg></a>
+                <a href="http://www.linkedin.com/in/dkanderson"><svg class="icon icon-linkedin" viewBox="0 0 32 32"><use xlink:href="#icon-linkedin"></use></svg></a>
+                <a href="http://github.com/dkanderson"><svg class="icon icon-github" viewBox="0 0 32 32"><use xlink:href="#icon-github"></use></svg></a>
+            </nav>
         </div>
     </footer>
     <div id="copy-wrap">
         <div class="container">
-            <span class="copy">&copy; Copyright 2014. Duane K. Anderson. | <a href="#">Terms of  Use</a></span>
+            <span class="copy">&copy; Copyright <?php echo the_date('Y'); ?>. Duane K. Anderson.</span>
         </div>
     </div>
 </section>
--- templates/home-page.php
+++ templates/home-page.php
@@ -3,16 +3,16 @@
     <div class="container sixteen columns">
         <?php 
             // Show off those skills
-            $args = array('post_type' => 'skill', 'posts_per_page' => 4 );
+            $args = array('post_type' => 'skill', 'posts_per_page' => 4, 'order' => 'ASC' );
             $mad_skills = new WP_Query($args);
             while( $mad_skills->have_posts()) :
             $mad_skills->the_post();
 
-            $svg_file = get_post_meta($post->ID, '_dka_svg', true);
+            $svg_name = get_post_meta($post->ID, '_dka_svg', true);
   
         ?>
         <div class="skill four columns">
-            <div class="icon-wrap"><?php include $svg_file; ?></div>
+            <div class="icon-wrap"><?php echo get_template_part('inline/icon', $svg_name.'.svg'); ?></div>
             <h2><?php the_title(); ?></h2>
             <p><?php echo get_the_content(); ?></p>
         </div>
